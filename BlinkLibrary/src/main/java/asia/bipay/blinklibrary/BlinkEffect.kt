1053587496148ce7f599faffe51a0e12b3d930e5
ZulfiLim <saudaranew20@gmail.com>
2022-08-19 15:10:45 +0700

    blink library added

---
+++ BlinkLibrary/src/main/java/asia/bipay/blinklibrary/BlinkEffect.kt
@@ -0,0 +1,22 @@
+package asia.bipay.blinklibrary
+
+import android.animation.ArgbEvaluator
+import android.animation.ObjectAnimator
+import android.graphics.Color
+import android.view.View
+import android.view.animation.Animation
+
+object BlinkEffect {
+
+    fun blink(view: View) {
+        val animator: ObjectAnimator = ObjectAnimator.ofInt(
+            view, "backgroundColor", Color.YELLOW,
+            Color.RED, Color.GREEN, Color.DKGRAY, Color.TRANSPARENT,
+        )
+        animator.duration = 500
+        animator.setEvaluator(ArgbEvaluator())
+        animator.repeatCount = Animation.REVERSE
+        animator.repeatCount = Animation.INFINITE
+        animator.start()
+    }
+}
